65daffaf262e1d590c533b5d55bbe7f622911888
sashok724 <sashok724@sashok724-PC>
2013-01-21 19:19:11 +0400

    Added main form

--- OmegaLauncher/src/net/sashok724/omega/LauncherStarter.java
+++ OmegaLauncher/src/net/sashok724/omega/LauncherStarter.java
@@ -1,8 +1,22 @@
 package net.sashok724.omega;
 
 import javax.swing.JFrame;
+import javax.swing.UIManager;
 
-public class LauncherStarter
+public class LauncherStarter implements LauncherConstants
 {
 	public static JFrame frame;
+	
+	public static void main(String[] args) throws Exception
+	{
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		frame = new JFrame("OmegaLauncher v" + VERSION + " - the cheat launcher");
+		frame.setSize(850, 500);
+		frame.setLocationRelativeTo(null);
+		frame.setContentPane(new LauncherPanel());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setIconImage(IMG_FAVICON);
+		frame.setResizable(false);
+		frame.setVisible(true);
+	}
 }
