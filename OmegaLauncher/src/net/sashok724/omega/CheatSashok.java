794c08c868f3c475dc158bf8fec5ab3399aa108c
sashok724 <sashok724@sashok724-PC>
2013-01-22 19:50:48 +0400

    Added launcher cracker

--- OmegaLauncher/src/net/sashok724/omega/CheatSashok.java
+++ OmegaLauncher/src/net/sashok724/omega/CheatSashok.java
@@ -16,6 +16,18 @@ public final class CheatSashok extends CheatProfile
 	@Override
 	public void onSearchFinished()
 	{
+		StringBuilder builder = new StringBuilder();
+		builder.append("======================== sashok724's launcher analyze ========================\n");
+		builder.append("Режим: Анализ лаунчера sashok724\n");
+		builder.append("Найденные сервера:\n");
+		for(String[] array : servers) builder.append("\tимя: '" + array[0] + "', адрес: '" + array[1] + ":" + array[2] + "'\n");
+		builder.append("Возможные ключи сессии (Это не может быть словом, скорее всего набор букв и цифр):\n");
+		for(String key : potencialKeys) builder.append("\t\'" + key + "'\n");
+		builder.append("Скорее всего ключом является:\n");
+		builder.append("\t'" + potencialKeys.get(potencialKeys.size()-1) + "'\n");
+		builder.append("==============================================================================");
+		LauncherPanel.instance.applyElements(LauncherPanel.analyzeElements);
+		LauncherPanel.analyzePane.setText(builder.toString());
 	}
 
 	@Override
@@ -37,7 +49,12 @@ public final class CheatSashok extends CheatProfile
 	@Override
 	public void onStringFound(String field, String string)
 	{
-		if(!field.startsWith("net.sashok724.launcher.run")) return;
+		if(field.contains("components")) return;
+		if(!field.startsWith("net.sashok724.launcher.")) return;
+		if(string.startsWith("ABCDEF")) return;
+		if(string.equals("=")) return;
+		if(string.equals("AppData")) return;
+		if(string.contains("%SERVERNAME%")) return;
 		potencialKeys.add(string);
 	}
 }
